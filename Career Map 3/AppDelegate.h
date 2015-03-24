a639358e5abe348220c8b9a6190104ce6c074a9b
Adel Shehadeh <adel.sh@gmail.com>
2015-03-24 11:35:13 +0200

    firt commit: basic tab bar controller and navigation controlle:

---
+++ Career Map 3/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Career Map 3
+//
+//  Created by Adel  Shehadeh on 3/24/15.
+//  Copyright (c) 2015 Adel  Shehadeh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
