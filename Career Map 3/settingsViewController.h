e1da55ffaef2d0d383a7fb76949c4a6935760ecd
Adel Shehadeh <adel.sh@gmail.com>
2015-03-29 21:12:27 +0300

    added logout basic functionality

---
+++ Career Map 3/settingsViewController.h
@@ -0,0 +1,15 @@
+//
+//  settingsViewController.h
+//  Career Map 3
+//
+//  Created by Adel  Shehadeh on 3/29/15.
+//  Copyright (c) 2015 Adel  Shehadeh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <Parse/Parse.h>
+
+@interface settingsViewController : UIViewController
+- (IBAction)logoutButtonPressed:(UIButton *)sender;
+
+@end
---
+++ Career Map 3/settingsViewController.m
@@ -0,0 +1,21 @@
+//
+//  settingsViewController.m
+//  Career Map 3
+//
+//  Created by Adel  Shehadeh on 3/29/15.
+//  Copyright (c) 2015 Adel  Shehadeh. All rights reserved.
+//
+
+#import "settingsViewController.h"
+
+@implementation settingsViewController
+
+- (IBAction)logoutButtonPressed:(UIButton *)sender {
+    
+    
+    [PFUser logOut];
+    [self dismissViewControllerAnimated:YES completion:nil];
+    
+    
+}
+@end
