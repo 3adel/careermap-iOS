2a36c95f011e9f0b540f0ecb0d3590b575df60da
Adel Shehadeh <adel.sh@gmail.com>
2015-06-10 16:04:45 +0300

    created basis filters views

---
+++ Career Map 3/JobsListFilterViewController.h
@@ -0,0 +1,14 @@
+//
+//  JobsListViewController.h
+//  Career Map 3
+//
+//  Created by Adel  Shehadeh on 6/10/15.
+//  Copyright (c) 2015 Adel  Shehadeh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JobsListFilterViewController : UIViewController
+- (IBAction)cancelFilterButtonPressed:(UIBarButtonItem *)sender;
+
+@end
